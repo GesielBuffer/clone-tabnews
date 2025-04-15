e64561e374eecd592834eb47b89438e7fbba530c
gesielbuffer <50589088+GesielBuffer@users.noreply.github.com>
2025-04-15 00:35:44 +0000

    atualiza a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Edinalva, eu amo você. Se vc me ama, dá uma risadinha!!!</h1>
+    return <h1>Edinalva, eu amo você. Se vc me ama, dá uma risadinha!!! 😍😎 </h1>
 }
 
 export  default Home;
