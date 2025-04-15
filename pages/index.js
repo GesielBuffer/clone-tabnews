9475c23e4dd3e1b8ddba57a81e68b065383d2e5e
gesielbuffer <50589088+GesielBuffer@users.noreply.github.com>
2025-04-15 00:50:38 +0000

    atualiza novamente a frase inicial com emoticon

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Edinalva, eu amo você. Se vc me ama, dá uma risadinha!!! 😍😎 </h1>
+    return <h1>Edinalva, eu amo você, mesmo você me irritando. Se vc me ama, dá uma risadinha e faz um pix 🫰🫰🫰 !!! 😍😎 </h1>
 }
 
 export  default Home;
